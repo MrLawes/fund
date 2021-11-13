c4173816ac407648ce16c61a27bf6ec2b4a10ec2
chenhaiou <haiou_chen@sina.cn>
2021-11-13 23:13:39 +0800

    u

--- fund/admin.py
+++ fund/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from fund.models import Fund
+from fund.models import Fund, FundValue
 
 
 @admin.register(Fund)
 class FundAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'code',)
-    fields = ('name', 'code',)
+    list_display = ('id', 'name', 'code', 'three_yearly_change',)
+    fields = ('name', 'code', 'three_yearly_change',)
+
+
+@admin.register(FundValue)
+class FundValueAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fund', 'deal_at', 'value', 'rate',)
+    fields = ('fund', 'deal_at', 'value', 'rate',)
--- fund/models.py
+++ fund/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
 
 
-
 class Fund(models.Model):
-
     name = models.CharField(max_length=64, verbose_name='基金名称', db_index=True, )
     code = models.CharField(max_length=64, verbose_name='代码', db_index=True, )
-    three_yearly_change = models.CharField(max_length=64, verbose_name='三年年化', db_index=True, default='')
+    three_yearly_change = models.IntegerField(max_length=64, verbose_name='三年年化', db_index=True, default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class FundValue(models.Model):
+    fund = models.ForeignKey(Fund, on_delete=models.CASCADE)
+    deal_at = models.DateField(verbose_name='交易时间', db_index=True, )
+    value = models.FloatField(verbose_name='单位净值', db_index=True, )
+    rate = models.FloatField(verbose_name='日增长率', db_index=True, )
