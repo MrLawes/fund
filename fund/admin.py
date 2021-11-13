86ff263306f7c90da9eb42985ef309cc56340576
chenhaiou <haiou_chen@sina.cn>
2021-11-13 23:00:00 +0800

    u

--- fund/admin.py
+++ fund/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from fund.models import Fund
+
+
+@admin.register(Fund)
+class FundAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'code',)
+    fields = ('name', 'code',)
--- fund/models.py
+++ fund/models.py
@@ -7,4 +7,5 @@ from django.db import models
 class Fund(models.Model):
 
     name = models.CharField(max_length=64, verbose_name='基金名称', db_index=True, )
-    code  = models.CharField(max_length=64, verbose_name='代码', db_index=True, )
+    code = models.CharField(max_length=64, verbose_name='代码', db_index=True, )
+    three_yearly_change = models.CharField(max_length=64, verbose_name='三年年化', db_index=True, default='')
