4f61fb8fbaf11148e55373d60d599046521f6b78
chenhaiou <haiou_chen@sina.cn>
2021-11-15 14:02:23 +0800

    u

--- fund/admin.py
+++ fund/admin.py
@@ -9,7 +9,7 @@ from fund.models import Fund, FundValue, FundExpense
 
 @admin.register(Fund)
 class FundAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'code', 'space_expense', 'three_yearly_change', 'hold_space',)
+    list_display = ('id', 'name', 'code', 'space_expense', 'expense', 'hope_expense', 'hold_space',)
 
     def hold_space(self, obj):
         expense = FundExpense.objects.filter(fund=obj).values_list('expense', flat=True)
@@ -18,6 +18,36 @@ class FundAdmin(admin.ModelAdmin):
 
     hold_space.short_description = '持有仓位'
 
+    def expense(self, obj):
+        expense = FundExpense.objects.filter(fund=obj).values_list('expense', flat=True)
+        return round(sum(expense), 2)
+
+    expense.short_description = '投入资金'
+
+    def hope_expense(self, obj):
+        """ 期望投入资金 """
+        max_value = max(obj.from_value, obj.to_value)
+        min_value = min(obj.from_value, obj.to_value)
+        fund_value = FundValue.objects.filter(fund=obj, ).order_by('deal_at').last()
+        expense, next_rate = 0, 0
+
+        if not fund_value:
+            return expense
+        if min_value < fund_value.value < max_value:
+            half = min_value + (max_value - min_value) / 2
+            duration = (max_value - half) / 10.0
+            # 购买的机会
+            if obj.from_value > obj.to_value:
+                stage = int((half - fund_value.value) / duration)
+                for i in range(1, stage + 1):
+                    if i != stage:
+                        expense += i * obj.space_expense
+                    else:
+                        next_rate = round(((half - stage * duration) - fund_value.value) / fund_value.value, 4) * 100
+                return f"{stage}/{(stage + 1) * obj.space_expense}/{next_rate}/{half - (stage + 1) * duration:0.04f}"
+        return expense
+
+    hope_expense.short_description = '本阶段/下阶段投入/下个阶段净值涨跌/下个阶段净值'
 
 @admin.register(FundValue)
 class FundValueAdmin(admin.ModelAdmin):
@@ -47,7 +77,6 @@ class FundExpenseAdmin(admin.ModelAdmin):
     list_display = ('id', 'fund_value', 'hold', 'expense', 'hold_value', 'hope_value')
     form = FundExpenseForm
 
-    # exclude = ('fund_value',)
 
     def hold_value(self, obj):
         last_fundvalue = FundValue.objects.filter(fund=obj.fund_value.fund).order_by('deal_at').last()
@@ -55,9 +84,9 @@ class FundExpenseAdmin(admin.ModelAdmin):
         hope_value = self.hope_value(obj=obj)
         # todo 写入数据库
         if hope_value > value:
-            color = 'red'
-        else:
             color = 'green'
+        else:
+            color = 'red'
 
         return format_html(f'<span style="color: {color};">{value}</span>')
 
@@ -73,5 +102,4 @@ class FundExpenseAdmin(admin.ModelAdmin):
         value = ((1 + day_change) ** days) * obj.expense
         value *= 1.0065
         return round(value, 2)
-
     hope_value.short_description = '期望市值'
--- fund/management/commands/chenhaiou.py
+++ fund/management/commands/chenhaiou.py
@@ -45,7 +45,6 @@ def income(基金名称, N年年化, 交易流水):
 
 
 def get_funds_info(fund_code):
-    return
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
     }
