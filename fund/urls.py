ec53aed480b9408885676e6627bc6d26943aec76
chenhaiou <haiou_chen@sina.cn>
2021-11-15 19:31:29 +0800

    u

--- fund/urls.py
+++ fund/urls.py
@@ -1,15 +1,16 @@
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
 
-# from rest_framework import routers
+from fund.views import FundViewSet
 
-# from fund.views import FundViewSet
+router = routers.DefaultRouter()
+router.trailing_slash = '/?'
 
-# router = routers.DefaultRouter()
-# router.trailing_slash = '/?'
-#
-# router.register(r'(?P<version>(v4))/fund', FundViewSet, base_name='fund')
+router.register(r'(?P<version>(v4))/fund', FundViewSet, )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
 ]
--- fund/views.py
+++ fund/views.py
@@ -1,11 +1,11 @@
-# from rest_framework.viewsets import ModelViewSet
-#
-# from fund.models import Fund
-# from fund.serializers import FundSerializer
-#
-#
-# class FundViewSet(ModelViewSet):
-#     permission_classes = ()
-#     authentication_classes = ()
-#     queryset = Fund.objects.all()
-#     serializer_class = FundSerializer
+from rest_framework.viewsets import ModelViewSet
+
+from fund.models import Fund
+from fund.serializers import FundSerializer
+
+
+class FundViewSet(ModelViewSet):
+    permission_classes = ()
+    authentication_classes = ()
+    queryset = Fund.objects.all()
+    serializer_class = FundSerializer
