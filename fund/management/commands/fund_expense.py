6b62a044c933369ce1215b254a4681063bcdb044
chenhaiou <haiou_chen@sina.cn>
2021-11-16 20:53:05 +0800

    u

---
+++ fund/management/commands/fund_expense.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+
+from fund.models import Fund, FundValue, FundExpense
+
+
+class Command(BaseCommand):
+    def handle(self, *_, **options):
+        data = {
+            '工银瑞信新能源汽车主题混合C': {
+                '交易流水': {
+                    '2021-10-23': 10,
+                }
+            },
+        }
+        for fund_name in data:
+            fund = Fund.objects.get(name=fund_name)
+            交易流水 = data[fund_name]['交易流水']
+            for date in 交易流水:
+                fund_value = FundValue.objects.get(fund=fund, deal_at=date)
+                hold = FundExpense.get_hold(fund_value=fund_value.value, expense=交易流水[date], fee=fund.fee)
+                defaults = {'expense': 交易流水[date], 'hold': hold}
+                FundExpense.objects.update_or_create(fund=fund, deal_at=date, defaults=defaults)
