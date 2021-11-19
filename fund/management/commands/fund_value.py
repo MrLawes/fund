f467c19baaf3b69d890b6729faacc3c7ccfff1e1
chenhaiou <haiou_chen@sina.cn>
2021-11-19 09:31:34 +0800

    u

--- fund/management/commands/fund_value.py
+++ fund/management/commands/fund_value.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
         }
         end_date = datetime.datetime.now()
-        start_date = (end_date - datetime.timedelta(days=15)).strftime('%Y-%m-%d')
+        start_date = (end_date - datetime.timedelta(days=30 * 4)).strftime('%Y-%m-%d')
         end_date = end_date.strftime('%Y-%m-%d')
         for fund in Fund.objects.all():
 
