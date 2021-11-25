14768143e36da4ca68319aac133516c689d04004
chenhaiou <haiou_chen@sina.cn>
2021-11-25 15:59:29 +0800

    u

--- fund/management/commands/fund_expense.py
+++ fund/management/commands/fund_expense.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.core.management.base import BaseCommand
 from tabulate import tabulate
 
@@ -8,7 +6,7 @@ from fund.models import Fund, FundValue, FundExpense
 
 class Command(BaseCommand):
     def handle(self, *_, **options):
-        # todo 整理所有的交易
+        # todo 整理所有的交易，三年年化
         data = {
 
             "[军工]鹏华空天军工指数(LOF)C": {
@@ -115,90 +113,33 @@ class Command(BaseCommand):
                 defaults = {'expense': detail[1], 'hold': hold}
                 FundExpense.objects.update_or_create(fund=fund, deal_at=detail[0], defaults=defaults)
 
-
-        def income(基金名称, N年年化, 交易流水):
-            """
-            基金名称:   新能源汽车
-            N年年化:    [316.31, 3 * 12 * 30]
-            交易流水:   [('2021-10-01', 10), ('2021-10-19', 200),]
-            """
-            天化, data = (1 + (N年年化[0] / 100.0)) ** (1 / (N年年化[1] * 1.0)) - 1, {}
-            交易流水.sort(key=lambda x: x[0])
-            总金额 = 总投入 = 0
-            开始交易时间 = datetime.datetime.strptime(交易流水[0][0], '%Y-%m-%d')
-            交易天数 = (datetime.datetime.now() - 开始交易时间).days
-            for 第几天 in range(0, 交易天数 + 1):
-                时间 = (开始交易时间 + datetime.timedelta(days=第几天)).strftime('%Y-%m-%d')
-                交易流水_slice = [i[:2] for i in 交易流水]
-                总投入 += dict(交易流水_slice).get(时间, 0)
-                总金额 += 总金额 * 天化 + dict(交易流水_slice).get(时间, 0)
-                # print(基金名称, 时间, 第几天, 总金额, 总投入)
-
-            可售 = 总金额 * 1.0065
-            期望收益 = 可售 - 总投入
-            持有收益率 = 期望收益 / 总投入
-
-            仓位 = f"{(总投入 * 10.0 / 7000):0.3f}"
-            try:
-                持有份额 = sum([i[2] for i in 交易流水])
-            except:
-                持有份额 = 0
-            return {
-                '时间': 时间,
-                '总投入': 总投入,
-                '可售': 可售,
-                '期望收益': 期望收益,
-                '持有收益率': 持有收益率,
-                '仓位': 仓位,
-                '基金名称': 基金名称,
-                '持有份额': 持有份额,
-            }
-
-        tabular_data, headers = [], ['投入金额/持有市值/期望市值', '       持有收益/期望收益', '          期望收益率',
-                                     '            持有仓位',
+        tabular_data, headers = [], ['投入金额/持有市值/期望市值', '     持有收益/期望收益', '        持有收益率/期望收益率',
                                      '             基金名称', ]
-        headers_strip = [h.strip() for h in headers]
         for 基金名称 in data:
-
-            result = income(基金名称, data[基金名称]['N年年化'], data[基金名称]['交易流水'], )
-
             fund = Fund.objects.get(name=fund_name)
-            print(f"{result=}")
             持有市值 = 投入金额 = 期望市值 = 0
-
             last_fundvalue = FundValue.objects.filter(fund=fund).order_by('deal_at').last()
             for fund_expense in FundExpense.objects.filter(fund=fund):
                 持有市值 += fund_expense.hold * last_fundvalue.value
                 投入金额 += fund_expense.expense
                 期望市值 += fund_expense.hope_value
             持有市值 = round(持有市值, 2)
-            value = 持有市值
+            持有收益 = 持有市值 - 投入金额
+            期望收益 = 期望市值 - 投入金额
+            持有收益率 = (持有市值 - 投入金额) / 投入金额
+            期望收益率 = (期望市值 - 投入金额) / 投入金额
 
-            #
-            # fund = Fund.objects.get(name=fund_name)
-            # last_fundvalue = FundValue.objects.filter(fund=fund).order_by('deal_at').last()
-            # value = round(obj.hold * last_fundvalue.value, 2)
-
-            持有收益 = value * result['持有份额'] - result['总投入']
-            持有收益率 = f"{result['持有收益率'] * 100:0.2f}%"
-
-            # 恒定市值区间 = data[基金名称].get('恒定市值区间')
-            # if 恒定市值区间 and value:
-            #     if 恒定市值区间[0] <= value <= 恒定市值区间[1]:
-            #         恒定市值区间差 = (恒定市值区间[1] - 恒定市值区间[0]) / 10.0
-            # 恒定市值 = (10 - (value - 恒定市值区间[0]) / 恒定市值区间差) * 7000 / 10
-            # else:
-            #     恒定市值 = 0
-
-            if 持有市值 > result['可售']:
+            if 持有市值 > 期望市值:
                 持有市值 = f'\033[0;30;41m{持有市值}\033[0m'
             else:
                 持有市值 = f'\033[0;30;42m{持有市值}\033[0m'
 
-            t_data = [f"{投入金额}/{持有市值}/{期望市值}",
-                      f"{持有收益:0.2f}/{result['期望收益']:0.2f}",
-                      持有收益率, result['仓位'],
-                      result['基金名称'], ]
+            t_data = [
+                f"{投入金额}/{持有市值}/{期望市值}",
+                f"{持有收益:0.2f}/{期望收益:0.02f}",
+                f"{持有收益率:0.02f}/{期望收益率:0.02f}",
+                基金名称,
+            ]
             tabular_data.append(t_data)
 
         print(tabulate(tabular_data=tabular_data, headers=headers, numalign='left'))
