e0b46df0a238d223ed3a31fe891e6380c91658a6
chenhaiou <haiou_chen@sina.cn>
2021-12-31 21:25:15 +0800

    u

--- fund/admin.py
+++ fund/admin.py
@@ -181,7 +181,7 @@ class FundExpenseAdmin(admin.ModelAdmin):
     buttons.short_description = "操作"
 
     def sum_hold(self, request, queryset):
-        hold = sum(queryset.values_list('hold', flat=True))
+        hold = sum(queryset.filter(expense_type='buy').values_list('hold', flat=True))
         self.message_user(request, f"共计 {hold:0.02f} 份")
 
     sum_hold.short_description = "计算份数"
