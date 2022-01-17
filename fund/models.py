900d25157af8fc6f2281536235df73393f1fc13b
chenhaiou <haiou_chen@sina.cn>
2022-01-17 15:45:50 +0800

    u

--- fund/models.py
+++ fund/models.py
@@ -65,7 +65,7 @@ class FundExpense(models.Model):
     split_hold = models.FloatField(verbose_name='拆分份额', default=0)
     sale_at = models.DateField(verbose_name='出售日期', default=None, null=True, blank=True, )
     is_buy_again = models.BooleanField(verbose_name='是否已经回购', default=False)
-    headimg = models.FileField(upload_to="img/", default='')
+    # headimg = models.FileField(upload_to="img/", default='')
 
     class Meta:
         verbose_name_plural = '基金购买记录'
