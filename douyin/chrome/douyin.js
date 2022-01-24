a56c7bd527705440bd9bb803adf6be67498fea57
chenhaiou <haiou_chen@sina.cn>
2022-01-24 11:19:21 +0800

    u

--- douyin/chrome/douyin.js
+++ douyin/chrome/douyin.js
@@ -60,7 +60,7 @@ function douyin_my_user() {
     for (i = 0; i < divs.length; i++) {
         if (divs[i].innerHTML == '关注') {
             // 1.17: 4224
-            let fens_count = 4735 - 2500
+            let fens_count = 5158 - 1500
             // let fens_count = divs[i].parentElement.nextElementSibling.children[1].innerText
             i = divs.length
             console.log(new Date() + ' 滚动 ' + fens_count + ' 次')
