3d4daf78bcd6523932191e05096d63f0a3f215b4
chenhaiou <haiou_chen@sina.cn>
2022-02-10 20:19:36 +0800

    u

--- huixi/models.py
+++ huixi/models.py
@@ -12,16 +12,16 @@ from rich.table import Table
 for person_index in range(1, 会员人数 + 1):
 
     开始时间 = datetime.datetime(2019, 9, 15)
-    最终金额 = 120000
+    最终金额 = 110000
     table = Table(title="")
     table.add_column("姓名", justify="left", no_wrap=True, )
     table.add_column("时间", justify="right", style="red", no_wrap=True)
     table.add_column("得会金额", justify="right", style="cyan", no_wrap=True)
     table.add_column("支出:付会金额+支付利息", justify="right", style="cyan", no_wrap=True)
     table.add_column(f"存入银行利息(月利息={月利息:0.010f})", justify="right", style="cyan", no_wrap=True)
-    table.add_column("最终金额（初始金额12万）", justify="right", style="red", no_wrap=True)
+    table.add_column("最终金额（初始金额11万）", justify="right", style="red", no_wrap=True)
 
-    for month in range(1, 会员人数 * 4 + 1):
+    for month in range(1, 会员人数 * 4 + 1 - 3):
 
         if (person_index - 1) * 4 == month - 1:
             是否得会 = True
@@ -55,6 +55,11 @@ for person_index in range(1, 会员人数 + 1):
 
         开始时间 = 开始时间 + datetime.timedelta(days=30)
         开始时间 = 开始时间.replace(day=15)
+
+    等价年利息 = (最终金额 - 110000) / 110000.0 + 1
+    等价年利息 = 等价年利息 ** (1.0 / month)
+    等价年利息 = 等价年利息 ** 12 - 1
+    table.add_row(f'会员[{person_index}] ', f'总结', f'', f'', f'', f'等价年利息={等价年利息:0.06f}')
 
     console = Console()
     console.print(table)
