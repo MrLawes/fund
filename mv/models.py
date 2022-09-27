5e88cb8e983d57de281bb1c23af6b6f15c8d495d
chenhaiou <haiou_chen@sina.cn>
2022-09-27 23:31:00 +0800

    u

--- mv/models.py
+++ mv/models.py
@@ -7,6 +7,7 @@ class Mv(models.Model):
     download_url = models.CharField(verbose_name='下载地址', max_length=1024)
     # 需要下载 -》下载中 -》下载完成修改文件名 -》下载完成
     action = models.CharField(verbose_name='行为', default='需要下载', max_length=200)
+    name = models.CharField(verbose_name='文件名字', default='', null=True, max_length=400)
 
 # for i in range(1, 22):
 #     Mv.objects.get_or_create(download_url=f'https://www.bilibili.com/video/BV1F64y1u7SQ?{i}')
