91c520adf8e2fc17e1fd4e3f417ce46695906c79
chenhaiou <haiou_chen@sina.cn>
2023-01-08 20:37:31 +0800

    u

--- timestamp_to_date/run.py
+++ timestamp_to_date/run.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 for root, dirs, files in os.walk("/Users/chenhaiou/Documents/陈海鸥手机"):
@@ -9,4 +10,9 @@ for root, dirs, files in os.walk("/Users/chenhaiou/Documents/陈海鸥手机"):
             continue
         timestamp = file_path.split('mmexport')[-1].split('.')[0]
         timestamp = timestamp.split('/')[-1]
-        print(f'{file_path=}; {timestamp=}')
+        timestamp = int(timestamp[:-3])
+        date = str(datetime.datetime.fromtimestamp(timestamp).date())
+        to_file_path = file_path.replace('月/', f'月/{date} ')
+        # print(f'{file_path=}; {timestamp=}; {to_file_path}')
+        os.rename(file_path, to_file_path)
+        # input()
