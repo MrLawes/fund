3779a2e27158c11cd5ff75415dd05a770241c676
chenhaiou <haiou_chen@sina.cn>
2023-02-01 21:22:54 +0800

    u

---
+++ iiilab/login/login.js
@@ -0,0 +1,14 @@
+setTimeout(function () {
+    run();
+}, 1000);
+
+function run() {
+    setInterval(function () {
+        caption = document.getElementsByClassName('caption')[0]
+        song = caption.children[0]
+        href = "https://www.baidu.com/s?wd=" + encodeURIComponent(song.innerHTML)
+        song.innerHTML = '<a href="' + href + '" target="_blank">' + song.innerHTML + '</a>'
+        console.log(song + ':song')
+    }, 1000)
+
+}
