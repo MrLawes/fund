a3dd637fb71aa20a33179037ebebe1fc24dbfebf
chenhaiou <haiou_chen@sina.cn>
2023-02-01 22:58:00 +0800

    u

--- iiilab/baike/login.js
+++ iiilab/baike/login.js
@@ -7,7 +7,6 @@ function run() {
         names = document.getElementsByClassName('basicInfo-item name')
         for (index = 0; index < names.length; index++) {
             name = names[index].innerHTML
-            console.log('name:' + name)
             if (name == '发行日期') {
                 release_time = names[index].nextElementSibling.textContent.split("\n")[1]
                 release_time = release_time.split('年')[0]
@@ -22,9 +21,11 @@ function run() {
             if (name == '所属专辑') {
                 if (names[index].nextElementSibling.children.length == 0) {
                     album = names[index].nextElementSibling.innerHTML
+                    console.log(11111)
                 } else {
                     album = names[index].nextElementSibling.children[0].innerHTML
                 }
+                album = album.replace('》', '').replace('《', '')
             }
             if (name == '中文名') {
                 song = names[index].nextElementSibling.innerHTML.replace('\n', '')
