a0f27dd87b80ee84f8609bfd26e66ca78f7a16d2
chenhaiou <haiou_chen@sina.cn>
2023-02-03 17:50:50 +0800

    u

--- cademy/admin.py
+++ cademy/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from cademy.models import MathCademy
 
 
 @admin.register(MathCademy)
 class MathCademyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'process', 'host')
+    list_display = ('id', 'name_with_host', 'process',)
     # 点击进入可编辑部分
     fields = ('name', 'process', 'host')
+
+    def name_with_host(self, obj):
+        return format_html(f'<a href="{obj.host}" target="_blank">{obj.name}</a>')
+
+    name_with_host.short_description = "网站名称"
