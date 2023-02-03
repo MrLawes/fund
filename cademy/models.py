5c527ebeeb58c30e32db26a36bc65f1bb41880a5
chenhaiou <haiou_chen@sina.cn>
2023-02-03 17:13:18 +0800

    u

--- cademy/models.py
+++ cademy/models.py
@@ -1,1 +1,22 @@
-# Create your models here.
+from django.db import models
+
+
+class Cademy(models.Model):
+    class Meta:
+        """ meta """
+        abstract = True
+
+    CATEGORY = (
+        (1, '数学'),
+    )
+    PROCESS = (
+        (1, '未开始学习'),
+        (2, '学习中'),
+        (3, '学习完成'),
+    )
+    name = models.CharField(max_length=64, verbose_name='基金名称', db_index=True, )
+    process = models.IntegerField(choices=PROCESS, default=1)
+
+
+class MathCademy(models.Model):
+    category = models.IntegerField(choices=Cademy.CATEGORY, default=1)
--- settings.py
+++ settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'douyin',
     'rest_framework',
     'corsheaders',
+    'cademy',
 ]
 
 MIDDLEWARE = [
