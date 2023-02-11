2b664300c8d7f5db85c416b915c658763b42913e
chenhaiou <haiou_chen@sina.cn>
2023-02-11 17:08:53 +0800

    u

--- fund/admin.py
+++ fund/admin.py
@@ -208,7 +208,7 @@ class FundExpenseAdmin(admin.ModelAdmin):
         # 长期A，超过第二个[]可以出售。短期C超过5%可以出售
         if obj.id == 3849:  # 半导体
             value = f'[1*1000][16000]{value}'
-        elif obj.id == 3854:  # 新能源
+        elif obj.id == 3874:  # 新能源
             value = f'[2*1000][12000]{value}'
         elif obj.id == 3841:  # 军工
             value = f'[1*1000][4000]{value}'
