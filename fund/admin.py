aef39b01e4cefd8ddfa0d93b7da87f476e85af23
chenhaiou <haiou_chen@sina.cn>
2023-03-09 12:26:53 +0800

    u

--- fund/admin.py
+++ fund/admin.py
@@ -214,8 +214,8 @@ class FundExpenseAdmin(admin.ModelAdmin):
             value = f'[恒定][6000]{value}'
         elif obj.id == 3834:  # 医疗
             value = f'[2*1000][18000]{value}'
-        elif obj.id == 3886:  # 白酒
-            value = f'[0*1000][7000]{value}'
+        elif obj.id == 3894:  # 白酒
+            value = f'[0*1000][7500]{value}'
         return format_html(f'<span style="color: {color};">{value}</span>')
 
     hold_value.short_description = '持有市值'
