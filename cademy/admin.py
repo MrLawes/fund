d6f404df6344c1862435461162379d68a1d117e6
chenhaiou <haiou_chen@sina.cn>
2023-03-24 16:51:14 +0800

    u

--- cademy/admin.py
+++ cademy/admin.py
@@ -6,7 +6,7 @@ from cademy.models import Cademy
 
 @admin.register(Cademy)
 class CademyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'category', 'name', 'host_with_link', 'process', 'remark',)
+    list_display = ('id', 'category', 'name', 'host_with_link', 'process', 'short_remark',)
     # 点击进入可编辑部分
     fields = ('name', 'category', 'every_day', 'process', 'host', 'remark',)
     search_fields = ['name', 'remark', ]
@@ -16,3 +16,10 @@ class CademyAdmin(admin.ModelAdmin):
         return format_html(f'<a href="{obj.host}" target="_blank">{obj.host}</a>')
 
     host_with_link.short_description = "网站地址"
+
+    def short_remark(self, obj):
+        if len(obj.remark) > 10:
+            return obj.remark[:50]
+        return obj.remark
+
+    host_with_link.short_remark = "描述"
