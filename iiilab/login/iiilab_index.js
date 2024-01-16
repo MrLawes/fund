f5e2807c7349a1deed1a848b16ca7bfc544f7654
chenhaiou <haiou_chen@sina.cn>
2024-01-16 12:35:42 +0800

    u

--- iiilab/login/iiilab_index.js
+++ iiilab/login/iiilab_index.js
@@ -13,9 +13,10 @@ function run() {
         if (caption) {
             song = caption.children[0]
             song_innerHTML = song.innerHTML
-            signer = location.search.split('singer=')[1].split('&')[0]
-            signer = decodeURIComponent(signer)
-            signer = signer.replace('【', '')
+            // signer = location.search.split('singer=')[1].split('&')[0]
+            // signer = decodeURIComponent(signer)
+            // signer = signer.replace('【', '')
+            signer = ""
             if (!song_innerHTML.includes('href')) {
                 song_innerHTML = song_innerHTML.split('|')[0].split('（')[0]
                 href = "https://www.baidu.com/s?wd=" + encodeURIComponent(signer + ' ' + song_innerHTML + ' 百度百科')
