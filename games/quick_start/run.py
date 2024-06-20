909573c70c8a903f4191a49ce93b7b91cc076b16
chenhaiou <haiou_chen@sina.cn>
2024-06-20 17:15:26 +0800

    u

--- games/quick_start/run.py
+++ games/quick_start/run.py
@@ -4,15 +4,11 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-player_pos_list = []
 IS_KEYDOWN = False
 IS_KEYUP = False
+screen.fill("purple")
 
-while running:
-
-    screen.fill("purple")
+while True:
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
@@ -22,12 +18,7 @@ while running:
         if IS_KEYDOWN and IS_KEYUP:
             IS_KEYDOWN = False
             IS_KEYUP = False
-            player_pos_list.append((random.randint(1, 1280), random.randint(1, 720)))
-
-    for player_pos in player_pos_list:
-        pygame.draw.ellipse(screen, "red", (player_pos[0], player_pos[1], 50, 70))
+            pygame.draw.ellipse(screen, "red", (random.randint(1, 1280), random.randint(1, 720), 50, 70))
 
     pygame.display.flip()
-    clock.tick(60)
-
-pygame.quit()
+    pygame.time.Clock().tick(60)
