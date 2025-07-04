e08eeda319d07654bc1a91ba652bf161e6656b56
chenhaiou <haiou_chen@sina.cn>
2025-07-04 09:04:17 +0800

    u

--- popmart/taobao/login.js
+++ popmart/taobao/login.js
@@ -4,13 +4,27 @@ setInterval(function () {
     const now = new Date();
     const hour = now.getHours();
     const minute = now.getMinutes();
+    const second = now.getSeconds(); // 获取当前秒数
     console.log(now)
     console.log(hour)
     console.log(minute)
-    if (hour === 21 && minute === 41) {
+    console.log(second)
+    if (hour === 21 && minute === 59 && second === 59) {
         login();
     }
-}, 1000);
+    if (hour === 22 && minute === 0 && second === 0) {
+        login();
+    }
+    if (hour === 22 && minute === 0 && second === 1) {
+        login();
+    }
+    if (hour === 22 && minute === 0 && second === 2) {
+        login();
+    }
+    if (hour === 22 && minute === 0 && second === 3) {
+        login();
+    }
+}, 500);
 
 
 function login() {
--- popmart/tmall/login.js
+++ popmart/tmall/login.js
@@ -2,7 +2,7 @@ console.log("匹配到扩展程序")
 
 setTimeout(function () {
     login();
-}, 1000);
+}, 500);
 
 function login() {
     document.getElementsByClassName("btn--QDjHtErD")[0].click()
