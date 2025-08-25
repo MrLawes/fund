3b6457033373ba76acfb01037f2e79a210d4745a
chenhaiou <haiou_chen@sina.cn>
2025-08-25 11:22:15 +0800

    u

--- fund/urls.py
+++ fund/urls.py
@@ -5,16 +5,12 @@ from rest_framework import routers
 
 from douyin.views import DouYinUserViewSet
 from fund.views import FundExpenseViewSet
-from sentry_performance.views import SentryUserViewSet
-from utools.views import UToolsViewSet
 
 router = routers.DefaultRouter()
 router.trailing_slash = '/?'
 
 router.register(r'(?P<version>(v4))/douyin/users', DouYinUserViewSet, )
 router.register(r'(?P<version>(v4))/fund_expense', FundExpenseViewSet, )
-router.register(r'(?P<version>(v4))/sentry', SentryUserViewSet, basename='sentry')
-router.register(r'(?P<version>(v4))/utools', UToolsViewSet, basename='utools')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
