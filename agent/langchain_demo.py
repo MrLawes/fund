88daf97fe12a1b5690766178580c2b4f2a9660c4
chenhaiou <haiou_chen@sina.cn>
2026-01-21 11:29:57 +0800

    u

--- agent/langchain_demo.py
+++ agent/langchain_demo.py
@@ -56,3 +56,31 @@ agent = initialize_agent(
 
 # print(agent.invoke({"input": "给我一些北京信息"}))
 print(agent.invoke({"input": "今天北京天气如何"}))
+# 和大语音模型交互:
+# 1. Answer the following questions as best you can. You have access to the following tools:
+# Weather(location) - 查询城市天气(中文),例如:'上海'、'北京'、'长沙'
+# Wikipedia(query) - 查询百科的介绍
+# Use the following format:
+# Question: the input question you must answer
+# Thought: you should always think about what to do
+# Action: the action to take, should be one of [Weather, Wikipedia]
+# Action Input: the input to the action
+# Observation: the result of the action
+# (this Thought/Action/Action Input/Observation can repeat N times)
+# Thought: I now know the final answer
+# Final Answer: the final answer to the original input question\n\nBegin!\n\nQuestion: 今天北京天气如何\nThought:"
+
+# 2. Answer the following questions as best you can. You have access to the following tools:
+# Weather(location) - 查询城市天气(中文),例如:'上海'、'北京'、'长沙'
+# Wikipedia(query) - 查询百科的介绍
+# Use the following format:
+# Question: the input question you must answer
+# Thought: you should always think about what to do
+# Action: the action to take, should be one of [Weather, Wikipedia]
+# Action Input: the input to the action
+# Observation: the result of the action
+# (this Thought/Action/Action Input/Observation can repeat N times)
+# Thought: I now know the final answer
+# Final Answer: the final answer to the original input question\n\nBegin!\n\nQuestion: 今天北京天气如何
+# Thought:Thought: 用户询问北京今天的天气，我需要使用天气查询工具。
+# Action: Weather  Action Input: 北京  Observation: 北京  \n的天气是大暴雨\nThought:
