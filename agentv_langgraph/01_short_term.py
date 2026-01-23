ee2fc25ddad8d3c3b67275c1c68c66ce4c6aabdd
chenhaiou <haiou_chen@sina.cn>
2026-01-23 16:54:51 +0800

    u

--- agentv_langgraph/01_short_term.py
+++ agentv_langgraph/01_short_term.py
@@ -1,84 +1,73 @@
 """
 文档: https://docs.langchain.com/oss/python/langgraph/add-memory#acdd-short-term-memory
 """
+from typing import Any
+from typing import List
 
-import os  # noqa
+from langchain.agents import create_agent
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import SystemMessage
+from langchain_core.messages import trim_messages
+from langchain_core.tools import tool
+from langgraph.checkpoint.redis import RedisSaver
 
-from langchain.agents import AgentType
-from langchain.agents import initialize_agent
-from langchain.memory import ConversationBufferMemory
-from langchain.tools import Tool
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-
-# 设置 API 密钥
-os.environ["OPENAI_API_KEY"] = "sk-7250f888346341c19a5f73f7a4e16a10"
-os.environ["OPENAI_BASE_URL"] = "https://api.deepseek.com"
-
-
-def get_weather(location):
-    # 修复逻辑：根据传入的位置返回相应的天气
-    if "北京" in location or "beijing" in location.lower():
-        return f"{location}的天气是晴天"
-    elif "上海" in location or "shanghai" in location.lower():
-        return f"{location}的天气是大暴雨"  # 修改为正确的天气
-    else:
-        return f"{location}的天气是多云"
-
-
-def get_wikipedia(query):  # noqa
-    return f"北京是中华人民共和国的首都，位于华北平原北部，是中国的政治、文化、国际交往和科技创新中心。"
-
-
-# 定义一个简单Prompt模板
-prompt = ChatPromptTemplate.from_template("""
-你是一位贴心的AI助手,现在和用户聊天。
-请根据对话历史和最新提问,给出自然、有帮助的回答。
-对话历史:
-{history}
-用户提问:
-{input}
-请回答:
-""")
-
-# 创建工具列表
-tools = [
-    Tool(
-        name="天气查询",
-        func=get_weather,
-        description="用于查询指定地点的天气情况"
-    ),
-    Tool(
-        name="维基百科",
-        func=get_wikipedia,
-        description="用于获取百科知识"
-    )
-]
-
-# 创建模型实例
-llm = ChatOpenAI(
+llm = init_chat_model(
     model="deepseek-chat",
     temperature=0,
-    max_tokens=None,
-    timeout=None,
-    max_retries=10,
+    base_url="https://api.deepseek.com",
+    api_key="sk-7250f888346341c19a5f73f7a4e16a10",
 )
 
-# 初始化带记忆功能的代理
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-agent_chain = initialize_agent(
-    tools=tools,
-    llm=llm,
-    agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION,
-    # verbose=True,
-    memory=memory,
-    prompt=prompt,
-)
 
-# 使用示例
-# print(agent_chain.run("北京的天气如何"))
-# print(agent_chain.run("上海的呢"))
+@tool("book_hotel", description="预定酒店的工具")
+def book_hotel(hotal_name: str) -> str:
+    return f"成功预定了在{hotal_name}的住宿"
 
-print(agent_chain.run("今天北京的天气如何?"))
-print(agent_chain.run("今天去旅游适合穿什么衣服?"))
-print(agent_chain.run("请告诉我：去这个地方有哪些景点推荐？"))
+
+def parse_message(messages: List[Any]) -> None:
+    print(f"{messages=}")
+    ...  # todo
+
+
+def save_graph_visualization(graph, filename: str = "graph.png") -> None:
+    print(f"{graph=}; {filename=}")
+    ...  # todo
+
+
+def pre_model_hook(state):
+    trimmed_messages = trim_messages(
+        messages=state.messages,
+        max_tokens=4,
+        strategy="last",
+        token_counter=len,
+        start_no="human",
+        include_system=True,
+        allow_partial=False,
+    )
+    return {"lls_input_messages": trimmed_messages}
+
+
+def run_ageny():
+    tools = [book_hotel]
+    system_message = SystemMessage(
+        content="你是一个AI助手",
+    )
+    # 基于数据库持久化存储的short-term
+    # db_uri = "postgresql://kevin:123456@localhost:54332/postgres?sslmode=disable"
+    db_uri = "redis://127.0.0.1:6379/10"
+    with RedisSaver.from_conn_string(db_uri) as checkpointer:
+        # checkpointer.setup()
+        agent = create_agent(
+            model=llm,
+            tools=tools,
+            prompt=system_message,
+            premodel_hook=pre_model_hook,
+            checkpointer=checkpointer,
+        )
+        save_graph_visualization(agent)
+        config = {"configuration": {"thread_id": 1, }}
+        user_input = "我叫什么"
+
+
+if __name__ == "__main__":
+    run_ageny()
