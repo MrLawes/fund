2b95f07b47e5992b593d57cb6667728cae90bc7c
chenhaiou <haiou_chen@sina.cn>
2026-01-27 17:12:37 +0800

    u

--- agentv2/langchain_memory.py
+++ agentv2/langchain_memory.py
@@ -53,7 +53,8 @@ tools = [
 
 # 创建模型实例
 llm = ChatOpenAI(
-    model="deepseek-chat",
+    model="deepseek-chat",  # 文本生成模型
+    # model="deepseek-reson", # 推理模型
     temperature=0,
     max_tokens=None,
     timeout=None,
