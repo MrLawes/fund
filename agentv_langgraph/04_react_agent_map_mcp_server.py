9f8e4d4eaaa7457cd8f48d03d61bba58125311ff
chenhaiou <haiou_chen@sina.cn>
2026-01-28 15:44:37 +0800

    u

--- agentv_langgraph/04_react_agent_map_mcp_server.py
+++ agentv_langgraph/04_react_agent_map_mcp_server.py
@@ -2,46 +2,37 @@
 # 深圳的天气如何
 
 """
-
-pip install langchain==0.1.0+
-pip install langchain-core==0.1.0+
-pip install langgraph==0.0.60+  # 或更高版本
-pip install langchain-mcp-adapters==0.0.1+  # 具体版本根据实际需求
-
-
-pip uninstall langchain langchain-core langchain-community -y
-pip install langchain-core==0.1.47
-pip install langchain-community==0.0.33
-pip install langchain==0.1.16
-pip install langgraph==0.0.63
-pip install langchain-mcp-adapters
-
-
-pip install langgraph==1.0.7
+pip uninstall langgraph langchain langchain-deepseek langchain-openai langchain-mcp-adapters -y
+pip install langgraph==0.0.60
 pip install langchain==1.2.6
 pip install langchain-deepseek==1.0.1
 pip install langchain-openai==1.1.7
-
+pip install langchain-mcp-adapters==0.1.13
 """
 
+import asyncio
 import os
 # from langchain.chat_models import init_chat_model
 from typing import Any
 from typing import List
 
+from langchain_core.messages import HumanMessage
 from langchain_core.messages import SystemMessage
 # from langgraph.prebuilt import create_react_agent
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain_openai import ChatOpenAI
-# from langchain_community.chat_models import init_chat_model
 from langgraph.checkpoint.memory import InMemorySaver
+# from langchain_community.chat_models import init_chat_model
+from langgraph.prebuilt import create_react_agent
+
+# agent = create_agent("openai:gpt-5", tools=tools)
 
 # from langchain.chat_models import init_chat_model
 
 # MCP 广场: https://modelscope.cn/mcp
 # 获取高德地图APIKey
 # AMAP_MAPS_API_KEY = os.getenv("AMAP_MAPS_API_KEY")
-AMAP_MAPS_API_KEY = "edfc412cca06d24d89a1da50887fb44a"
+AMAP_MAPS_API_KEY = "edfc412cca06d24d8xxxx9a1da50887fb44a"
 
 # 设置 API 密钥
 os.environ["OPENAI_API_KEY"] = "sk-7250f888346341c19a5f73f7a4e16a10"
@@ -76,7 +67,7 @@ def parse_messages(messages: List[Any]) -> None: ...
 def save_graph_visualization(graph, filename: str = "graph.png") -> None: ...
 
 
-def run_agent():
+async def run_agent():
     # 实例化MCP Server客户端
     client = MultiServerMCPClient({
         # 高德地图MCP Server
@@ -87,7 +78,7 @@ def run_agent():
     })
 
     # 从MCP Server中获取可提供使用的全部工具
-    tools = client.get_tools()
+    tools = await client.get_tools()
     print(f"tools:{tools}\n")
 
     # 基于内存存储的short-term
@@ -96,7 +87,36 @@ def run_agent():
     # 定义系统消息,指导如何使用工具
     system_message = SystemMessage(content="你是一个AI助手,使用高德地图工具获取信息。")
 
+    # 创建 ReAct风格的 agent
+    # agent = create_agent(
+    #     model=llm,
+    #     tools=tools,
+    #     system_prompt=system_message,
+    #     checkpointer=checkpointer,
+    # )
+    # 创建 ReAct风格的 agent
+    agent = create_react_agent(
+        model=llm,
+        tools=tools,
+        prompt=system_message,
+        checkpointer=checkpointer
+    )
+
+    # 将定义的agent的graph进行可视化输出保存至本地
+    # save_graph_visualization(agent)
+    # 定义short-term需使用的thread_id
+    config = {"configurable": {"thread_id": "1"}}
+
+    # 1、非流式处理查询
+    # 高德地图接口测试
+    agent_response = await agent.ainvoke(
+        {"messages": [HumanMessage(content="这个114.05571,22.52245经纬度对应的地方是哪里")]},
+        config=config,
+    )
+    parse_messages(agent_response['messages'])
+    agent_response_content = agent_response["messages"][-1].content
+    print(f"agent_response:{agent_response_content}")
+
 
 if __name__ == "__main__":
-    run_agent()
-    # asyncio.run(run_agent())
+    asyncio.run(run_agent())
