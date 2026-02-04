8d646c7f8ddd8ff6da94aaf6761b15a552321343
chenhaiou <haiou_chen@sina.cn>
2026-02-04 17:12:06 +0800

    u

--- agentv_langgraph/02_long_term.py
+++ agentv_langgraph/02_long_term.py
@@ -1,22 +1,21 @@
 """
 文档: https://docs.langchain.com/oss/python/langgraph/add-memory#acdd-short-term-memory
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 """
-from typing import Any
-from typing import List
 
-from langchain.agents import create_agent
 from langchain.chat_models import init_chat_model
-from langchain_community.storage import RedisStore
+from langchain_core.messages import HumanMessage
 from langchain_core.messages import SystemMessage
 from langchain_core.messages import trim_messages
 from langchain_core.tools import tool
-from langgraph.checkpoint.redis import RedisSaver
+from langgraph.checkpoint.postgres import PostgresSaver
 from langgraph.prebuilt import create_react_agent
+from langgraph.store.postgres import PostgresStore
 
 llm = init_chat_model(
     model="deepseek-chat",
     temperature=0,
-    base_url="https://api.deepseek.com",
+    base_url="https://api.deepseek.com/v1",
     api_key="sk-7250f888346341c19a5f73f7a4e16a10",
 )
 
@@ -26,23 +25,13 @@ def book_hotel(hotal_name: str) -> str:
     return f"成功预定了在{hotal_name}的住宿"
 
 
-def parse_message(messages: List[Any]) -> None:
-    print(f"{messages=}")
-    ...  # todo
-
-
-def save_graph_visualization(graph, filename: str = "graph.png") -> None:
-    print(f"{graph=}; {filename=}")
-    ...  # todo
-
-
 def pre_model_hook(state):
     trimmed_messages = trim_messages(
-        messages=state.messages,
+        messages=state["messages"],
         max_tokens=4,
         strategy="last",
         token_counter=len,
-        start_no="human",
+        start_on="human",
         include_system=True,
         allow_partial=False,
     )
@@ -54,29 +43,40 @@ def run_ageny():
     system_message = SystemMessage(
         content="你是一个AI助手",
     )
-    # 基于数据库持久化存储的short-term
-    # db_uri = "postgresql://kevin:123456@localhost:54332/postgres?sslmode=disable"
-    db_uri = "redis://127.0.0.1:6379/10"
-    with RedisSaver.from_conn_string(db_uri) as checkpointer:
-        with RedisStore.from_conn_string(db_uri) as store:
-            store.setup()
+
+    # 基下做摆库持久化存储的short-term
+    db_uri = "postgresql://postgres:admin@localhost:5432/postgres?sslmode=disable"
+    # mac: brew install postgresql
+    with PostgresSaver.from_conn_string(db_uri) as checkpointer:
+        with PostgresStore.from_conn_string(db_uri) as store:
             checkpointer.setup()
-            agent = create_react_agent(
-                model=llm,
+            store.setup()
+
+            agent = create_react_agent(  # noqa
+                model=llm,  # noqa
                 tools=tools,
                 prompt=system_message,
-                premodel_hook=pre_model_hook,
-                checkpointer=checkpointer,  # 短期记忆, 当前线程的信息
-                store=store,  # 长期记录, 记录用户个人偏好
+                pre_model_hook=pre_model_hook,
+                checkpointer=checkpointer,  # 短期记忆
+                store=store,  # 长期记忆
             )
-            save_graph_visualization(agent)
-            config = {"configuration": {"thread_id": 1, "user_id": "1", }}
-            user_id = "1"
+            config = {"configurable": {"thread_id": 1, "user_id": "陈海鸥"}}
+            user_id = config["configurable"]["user_id"]
             namespace = {"memories": user_id}
             memories = store.search(namespace, query="")
             info = " ".join([d.value["data"] for d in memories]) if memories else "无长期记忆信息"
             user_input = f"预定一个汉庭酒店,我的附加信息有:{info}"
-            agent.run(user_input, config=config)
+            print(f"{user_input=}")
+
+            # user_input = "我叫什么"
+            # user_input="我是kevin"
+            # user_input="我叫什么"
+            # user_input="预定一个汉庭酒店"
+            # user_input = f"我叫什么"
+            agent_response = agent.invoke({"messages": [HumanMessage(content=user_input)]},
+                                          config=config)  # noqa
+            agent_response_content = agent_response["messages"][-1].content
+            print(f"{agent_response_content=}")
 
 
 if __name__ == "__main__":
